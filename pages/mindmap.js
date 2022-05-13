55e326890be56aebdb71d133401dbb403117523a
VadimTs12 <tsurkanuvadim92@gmail.com>
2022-05-13 15:57:02 +0300

    edit pages height full mindmap

--- pages/mindmap.js
+++ pages/mindmap.js
@@ -15,7 +15,7 @@ const MindMap = () => {
     const lang = (locale === "en") ? en :
         locale === "ru" ? ru : cn
     return (
-        <div className={'wrapper_mind-map relative'} id={'wrapper_mind-map'}>
+        <div className={'wrapper_mind-map relative h-full'} id={'wrapper_mind-map'}>
             <Image src={bgMindmap}
                    alt="Picture of the author"
                    layout='fill'
@@ -24,7 +24,7 @@ const MindMap = () => {
             />
             <div className="absolute w-screen lg:h-screen top-0 bottom-0 bg-white/10 dark:bg-neutral-900/20"></div>
             <MainComponent title={'MindMap'} textColor={'text-white'} headerStyle={headerStyle}>
-                <div className="lg:px-8 pt-24 min-h-screen  md:px-5 relative">
+                <div className="lg:px-8 pt-24   md:px-5 relative h-full">
                     <div className="w-full h-full flex items-end px-1 fade-in">
                         <div className="h-full pb-6 px-4 lg:px-0">
                             <h3 className="font-800 top-0 font-sans text-4xl lg:text-6xl uppercase pb-4  font-black tracking-tight">{lang.mindmap.title}</h3>
